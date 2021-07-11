6a19dd9f35d9218b7df04f9873d73dbe1daaa00d
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-11 15:47:30 +0530

    Create charPattern.java

---
+++ charPattern.java
@@ -0,0 +1,9 @@
+public class charPattern {
+    public static void main(String[] args){
+        for (char c = 'A'; c <= 'E'; c++){
+            for (char i = 'A'; i <= 'E'; i++)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
